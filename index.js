4e65de42b635547ecdb2527fe2bacc36c20eb722
Darío Fernández <dariofedes@protonmail.com>
2021-04-14 14:18:29 +0200

    initializing server

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const { env: { PORT = 8080, MONGODB_URL }} = process
+
+const express = require('express')
+const { getPhones } = require('./routes')
+
+const { mongoose } = require('./data')
+
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const app = express()
+
+        app.get('/phones', getPhones)
+
+        app.listen(PORT, () => console.log(`🚀 server up and running on port ${PORT}`))
+
+        process.on('SIGINT', () => {
+            console.log('🛑 server abruptly stopped')
+
+            process.exit(0)
+        })
+    })
