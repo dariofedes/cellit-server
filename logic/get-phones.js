d5606609fca1e92ca6f434eaa41e53518e00ba64
Darío Fernández <dariofedes@protonmail.com>
2021-04-15 10:41:03 +0200

    adding href to getPhones logic response

--- logic/get-phones.js
+++ logic/get-phones.js
@@ -2,7 +2,7 @@ const { models: { Phone } } = require('../data')
 const { sanitize } = require('../utils')
 
 module.exports = async function getPhones() {
-    const phones = await Phone.find()
+    const phones = await Phone.find().select('name price manufacturer image_url').lean()
 
-    return phones.map(phone => sanitize(phone))
+    return phones.map(phone =>  sanitize({...phone, href: `phones/${phone._id}`}))
 }
