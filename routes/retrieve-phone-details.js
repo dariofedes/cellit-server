7f52de4a138bcbe51a3136dbf62f63780abdbafc
Darío Fernández <dariofedes@protonmail.com>
2021-04-15 18:58:48 +0200

    removing debugger

--- routes/retrieve-phone-details.js
+++ routes/retrieve-phone-details.js
@@ -3,7 +3,6 @@ const { retrievePhoneDetails } = require('../logic')
 module.exports = async (req, res) => {
     const { params: { id } } = req
     try {
-        debugger
         const phone = await retrievePhoneDetails(id)
 
         res.status(200).json(phone)
